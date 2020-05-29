4bee3c9b9c8f0c77e2a504b7611640ad33437ebc
D37ERTER <43070341+D37ERTER@users.noreply.github.com>
2020-05-29 15:29:07 +0200

    Update testy.cpp

--- testy.cpp
+++ testy.cpp
@@ -16,11 +16,10 @@ string pliki[9][2][2]=
 	{{"wyniki/hamilton90ns.txt","wyniki/euler90ns.txt"},{"wyniki/hamilton90s.txt","wyniki/euler90s.txt"}}
 };
 
-void zapisz(long long pomiary[], int naglowek, string nazwa, int iloscPomiarow)
+void zapisz(long long int pomiary[], string nazwa, int iloscPomiarow)
 {
 	ofstream zapis;
 	zapis.open(nazwa, ios_base::app);
-	zapis << naglowek << '\t';
 	for (int i=0;i<iloscPomiarow;i++)
 		zapis<<pomiary[i]<<'\t';
 	zapis << '\n';
@@ -29,10 +28,11 @@ void zapisz(long long pomiary[], int naglowek, string nazwa, int iloscPomiarow)
 
 void test()
 {
-    long long pomiary[100];
+    long long int pomiary[100];
     /*
         test nr 0 -
-
+            pamiec : n*b <= 524 288
+            czas : (n*b)^2
         test nr 1 -
             n wplywa na czas i (pewnie na pamiec tez ale komu chce sie czekac) O(n*logn)
             b, maxRozmar, maxWartosc nie wpywaja na czas i pamiec
@@ -45,31 +45,55 @@ void test()
 
     */
     //parametry poczatkowe
-    int maxRozmiar = 16384;
+    int maxRozmiar = 16;
     int maxWartosc = 16384;
-    //int wartoscB = 67108864;
-    int testNumer = 2;
+    //int wartoscB = 256;
+    int testNumer = 0;
 
     //test
-    for(int wartoscB = 128; wartoscB<=268435456; wartoscB*=2)
+    for(int wartoscB = 100; wartoscB<=8192; wartoscB+=100)
     {
         int poprzedniCzas = 0;
         int indexPom=0;
-        for(int wartoscN = 11; wartoscN<=17; wartoscN++)
+        for(int wartoscN = 100; wartoscN<=8192; wartoscN+=100)
         {
-            cout << wartoscB << " " << wartoscN << ": ";
-            utworzLosowoTest(wartoscN, wartoscB, maxRozmiar, maxWartosc);
-            start = chrono::steady_clock::now();
-            rozwTest[testNumer]();
-            endx = chrono::steady_clock::now();
-            pomiary[indexPom++] = chrono::duration_cast<chrono::nanoseconds>(endx - start).count();
-            cout << "czas : " << pomiary[indexPom-1] << "ns " ;
-            cout << "stosunek: " << (poprzedniCzas? ((pomiary[indexPom-1]/100000)/((double)(poprzedniCzas))) :0);
-            poprzedniCzas = pomiary[indexPom-1]/100000;
-            usunPlecak();
-            cout << endl;
+            if(wartoscB*wartoscN <= 510000 && wartoscB*wartoscN>=10000)
+            {
+                cout << wartoscB << " " << wartoscN << ": ";
+                long long suma = 0;
+                long long czasy[100];
+                for(int i=0; i<100; i++)
+                {
+                    utworzLosowoTest(wartoscN, wartoscB, maxRozmiar, maxWartosc);
+                    start = chrono::steady_clock::now();
+                    rozwTest[testNumer]();
+                    endx = chrono::steady_clock::now();
+                    czasy[i] = chrono::duration_cast<chrono::nanoseconds>(endx - start).count();
+                    suma += czasy[i];
+
+                }
+                double odchylenie = 0;
+                double t;
+                cout << endl;
+                for(int i=0; i<100; i++)
+                {
+                    if(wartoscN==100 && wartoscB==100)
+                        cout << czasy[i] << endl;
+                    t = czasy[i]-suma/100.0;
+                    odchylenie += t*t;
+                }
+                cout << endl;
+                pomiary[indexPom++] = suma/100.0;
+                cout << "czas : " << pomiary[indexPom-1] << "ns " ;
+                cout << "stosunek: " << (poprzedniCzas? ((pomiary[indexPom-1]/100000)/((double)(poprzedniCzas))) :0) << " ";
+                poprzedniCzas = pomiary[indexPom-1]/100000;
+                pomiary[indexPom++] = sqrt(odchylenie/100.0);
+                cout << "odchylenie : " << pomiary[indexPom-1] ;
+                usunPlecak();
+                cout << endl;
+            }
         }
-        zapisz(pomiary, 0, "test2v2.txt", indexPom);
+        zapisz(pomiary, "test0.txt", indexPom);
     }
     cout << "koniec testu" << endl;
 }
